562ce24c68a8355a95e28a2b6d5dc39d53c145d3
irfan-shams <shams.irfan@gmail.com>
2024-08-27 20:07:31 +0500

    Added ftnd the larget number file

---
+++ find_largest_number.dart
@@ -0,0 +1,13 @@
+void main() {
+  int a = 70;
+  int b = 90;
+  int c = 30;  
+  
+  if (a > b && a > c) {
+    print("A is the largest number: $a");
+  } else if (b > a && b > c) {
+    print("B is the largest number: $b");
+  } else {
+    print("C is the largest number: $c");
+  }    
+}
