c7064cf0e0a2b206cbff8c423d8c1a6dbc3682c1
irfan-shams <shams.irfan@gmail.com>
2024-08-27 22:08:55 +0500

    Added Check Positive, Negative or Zero file

---
+++ positive_negative_number.dart
@@ -0,0 +1,21 @@
+void main() {
+  int number = 0; 
+
+    if (number > 0) {
+        print("Number is POSITIVE: $number");
+    }
+    if(number < 0) {
+        print("Number is NEGATIVE: $number");
+    }
+    if(number == 0) {
+        print("Number is ZERO: $number");
+    }
+  
+  // if (number > 0) {
+  //   print("Postive Number: $number");
+  // } else if (number < 0) {
+  //   print("Negative Number: $number");
+  // } else {
+  //   print("Given Number: $number");
+  // }    
+}
