f7aafd0af5c127e64d0c894b4037a11aa92272bc
irfan-shams <shams.irfan@gmail.com>
2024-08-28 14:04:07 +0500

    Added marjsheet.dart file

---
+++ marksheet.dart
@@ -0,0 +1,14 @@
+void main() {  
+  int subject_1 = 91;
+  int subject_2 = 79;
+  int subject_3 = 87;
+  int subject_4 = 80;
+
+  int totalMarks = 400;
+
+  int obtainMarks = subject_1 + subject_2 + subject_3 + subject_4;
+
+  double percentage = (obtainMarks / totalMarks) * 100;
+
+  print("${obtainMarks}" + " out of " + "${totalMarks}" + " with " + "${percentage}" + "%");
+}
