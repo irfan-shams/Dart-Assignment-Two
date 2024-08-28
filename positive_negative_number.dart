f22e6f6292bda2c3b3ce531de64992e4600b87b6
irfan-shams <shams.irfan@gmail.com>
2024-08-28 21:21:14 +0500

    Updated

--- positive_negative_number.dart
+++ positive_negative_number.dart
@@ -1,21 +1,11 @@
 void main() {
-  int number = 0; 
-
-    if (number > 0) {
-        print("Number is POSITIVE: $number");
-    }
-    if(number < 0) {
-        print("Number is NEGATIVE: $number");
-    }
-    if(number == 0) {
-        print("Number is ZERO: $number");
-    }
+  int number = 50;     
   
-  // if (number > 0) {
-  //   print("Postive Number: $number");
-  // } else if (number < 0) {
-  //   print("Negative Number: $number");
-  // } else {
-  //   print("Given Number: $number");
-  // }    
+  if (number > 0) {
+    print("Number is POSITIVE: $number");
+  } else if (number < 0) {
+    print("Number is NEGATIVE: $number");
+  } else {
+    print("Number is ZERO: $number");
+  }    
 }
